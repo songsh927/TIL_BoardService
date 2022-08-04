c0be678c4638283a15ee3df1ac19aeb47f068d61
songsh927 <songsh96927@gmail.com>
2022-08-04 15:51:59 +0900

    22.08.04

--- src/main/java/com/til/boardservice/boardservice/controller/ContentController.java
+++ src/main/java/com/til/boardservice/boardservice/controller/ContentController.java
@@ -4,10 +4,10 @@ import com.til.boardservice.boardservice.domain.Content;
 import com.til.boardservice.boardservice.service.ContentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ContentController {
@@ -37,6 +37,16 @@ public class ContentController {
         List<Content> contents = contentService.findAllContents();
         model.addAttribute("contents", contents);
         return "home.html";
+    }
+
+    @GetMapping("/contents/view")
+    public String contentForm(Model model,String id) throws Exception{
+
+        Optional<Content> result = contentService.findContentById(Long.parseLong(id));
+        System.out.println(result.getClass().getSimpleName());
+        model.addAttribute("content", result.get());
+
+        return "content.html";
     }
 
 }
